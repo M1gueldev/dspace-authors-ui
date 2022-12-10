66e9eec7bae511d05f47d7b8f31a089d1d415eda
Miguel <m16uel123@gmail.com>
2022-12-09 20:38:37 -0400

    server auth constants

---
+++ src/app/authors/utils.ts
@@ -0,0 +1,27 @@
+export interface Author {
+  id: string
+  name: string
+  about: string
+  photo: Buffer
+  photoType: string
+}
+
+export interface AuthorRequest {
+  id: string
+  name: string
+  about: string
+  photo: string
+  photoType: string
+}
+
+const SERVER = 'http://localhost:8000/authors';
+
+export const localServer = {
+  server: SERVER,
+  create: SERVER.concat('/create'),
+  //get: SERVER.concat('?id='),
+  get: SERVER.concat('/get?id='),
+  update: SERVER.concat('/update'),
+  delete: SERVER.concat('/delete'),
+  all: SERVER.concat('/'),
+};
