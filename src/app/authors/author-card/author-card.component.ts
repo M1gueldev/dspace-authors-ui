98fd696034a1a91ea316a5bb93f471d7a225fd92
Miguel <m16uel123@gmail.com>
2022-12-09 20:57:35 -0400

    CArd component

--- src/app/authors/author-card/author-card.component.ts
+++ src/app/authors/author-card/author-card.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostBinding, Input, OnInit} from '@angular/core';
+import {Author, AuthorsPath} from '../utils';
+import MarkdownIt from 'markdown-it';
 
 @Component({
   selector: 'ds-author-card',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./author-card.component.scss']
 })
 export class AuthorCardComponent implements OnInit {
-
-  constructor() { }
+  @HostBinding('class') class = 'panel panel-default';
+  @Input() author: Author;
+  url = '';
+  about = '';
+  link = '';
 
   ngOnInit(): void {
+    const md = new MarkdownIt();
+    this.url = this.author.photoType.concat(',', this.author.photo.toString());
+    this.about = md.render(this.author.about).substring(0,255).concat('...');
+    this.link = AuthorsPath.concat('/', this.author.id);
   }
 
 }
