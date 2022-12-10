7e2fb56eb0813903ad03a047abbf333b3a70c8d8
Miguel <m16uel123@gmail.com>
2022-12-09 22:10:31 -0400

    Get all and by Id added

--- src/app/authors/authors-service.service.ts
+++ src/app/authors/authors-service.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import {Author, AuthorRequest, localServer} from './utils';
+import {map} from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import {Buffer} from 'buffer';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthorsServiceService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+  getAll() {
+    return this.http.get<AuthorRequest[]>(localServer.all)
+      .pipe(map(value => value.map(x => this.ParseRequest(x) )));
+  }
+  getById(id: string) {
+    return this.http.get<AuthorRequest>(localServer.get.concat(id))
+      .pipe(map(value => this.ParseRequest(value)));
+  }
+
+  private ParseRequest = (a: AuthorRequest): Author => {
+    return {
+      ...a,
+      photo: Buffer.from(!a.photo ? '' : a.photo)
+    };
+  };
+
 }
