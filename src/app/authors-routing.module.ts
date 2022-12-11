4a16ae5422d5bbffe56a7c8aa75a084877302fda
Miguel <m16uel123@gmail.com>
2022-12-10 20:06:56 -0400

    Create and Edit Paths ROuter

--- src/app/authors-routing.module.ts
+++ src/app/authors-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {AuthorsRoute} from './authors/utils';
 import {AuthorsDetailComponent} from './authors/authors-detail/authors-detail.component';
 import {AuthorsPageComponent} from './authors/authors-page/authors-page.component';
+import {AuthorCreateComponent} from "./authors/author-create/author-create.component";
+import {AuthorEditComponent} from "./authors/author-edit/author-edit.component";
 
 const routes: Routes = [
   {
@@ -13,6 +14,14 @@ const routes: Routes = [
     path: 'all',
     component: AuthorsPageComponent
   },
+  {
+    path: 'new',
+    component: AuthorCreateComponent
+  },
+  {
+    path: 'edit/:id',
+    component: AuthorEditComponent
+  }
 ];
 
 @NgModule({
